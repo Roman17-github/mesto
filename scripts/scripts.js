3d47b87723ea858fbf0f090653eed8cd08cf2eea
Roman <romariromanov@yandex.ru>
2021-05-23 11:02:32 +0300

    исправлены комментарии ревьюера

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,16 +2,13 @@ const openPopup = document.querySelector('.profile__edit');
 const popup = document.querySelector('.popup');
 const closePopup = popup.querySelector('.popup__close');
 const profileInfo = document.querySelector('.profile-info');
-const editSave = popup.querySelector('.popup__submit');
+const editSave = document.querySelector('[name="form"]');
 let profileName = document.querySelector('.profile__name');
 let profileSubline = document.querySelector('.profile__subline')
-let inputName = document.querySelector('.popup__name');
-let inputSubline = document.querySelector('.popup__subline');
-inputName.value = 'Жак-Ив Кусто';
-inputSubline.value = 'Исследователь океана';
-
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
+let inputName = document.querySelector('[name="Name"]');
+let inputSubline = document.querySelector('[name="Subline"]');
+inputName.value = profileName.textContent;
+inputSubline.value = profileSubline.textContent;
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -21,6 +18,9 @@ function editProfile(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileSubline.textContent = inputSubline.value;
-    
-}
-editSave.addEventListener('click', editProfile);
+    togglePopup();
+};
+
+openPopup.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
+editSave.addEventListener('submit', editProfile);
