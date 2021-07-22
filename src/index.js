8c3f359037583db46bb8b8ee945c5f3f99976e17
Roman <romariromanov@yandex.ru>
2021-07-22 17:31:54 +0300

    вебпак 8 работа

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import * as constants from "./constants.js";
-import Card from "./Card.js";
-import FormValidator from "./FormValidator.js";
-import Section from "./Section.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
-import PopupWithImage from "./PopupWithImage.js";
+import * as constants from "../scripts/constants.js";
+import Card from "../scripts/Card.js";
+import FormValidator from "../scripts/FormValidator.js";
+import Section from "../scripts/Section.js";
+import PopupWithForm from "../scripts/PopupWithForm.js";
+import UserInfo from "../scripts/UserInfo.js";
+import PopupWithImage from "../scripts/PopupWithImage.js";
+import '../pages/index.css';
 
 const formValidatorEdit = new FormValidator(
   constants.config,
